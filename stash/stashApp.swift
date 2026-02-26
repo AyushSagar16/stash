9a448aadb9c9d5bd80ecce5ccffc47f7f469875c
Ayush Sagar <ayushsagar3018@gmail.com>
2026-02-25 23:58:55 -0500

    Initial Commit

---
+++ stash/stashApp.swift
@@ -0,0 +1,17 @@
+//
+//  stashApp.swift
+//  stash
+//
+//  Created by Ayush Sagar on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct stashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
